5277c0bd044a9945e3995ed71a7dc0372e355724
Louis Goddard <louisgoddard@gmail.com>
2021-02-04 12:23:54 +0000

    Scrape Commission website

--- detector.py
+++ detector.py
@@ -1,57 +1,40 @@
 import boto3, os, csv, requests, re, json, urllib.parse
 from datetime import datetime
+from lxml import etree
 
-def lambda_handler(event, context):
-    check_meetings('European Commission')
-    check_meetings('European Parliament')
+def join_with_and(l):
+    return ' and '.join([', '.join(l[:-1]), l[-1]] if len(l) > 2 else l)
 
-def check_meetings(institution):
-    if institution == 'European Commission':
-        r = requests.get('https://www.integritywatch.eu/data/meetings.csv')
-    elif institution == 'European Parliament':
-        r = requests.get('https://www.integritywatch.eu/data/mepmeetings/mepmeetings.csv')
-
-    r.encoding = 'utf-8'
-
-    with open('data/guests.csv', 'r') as i:
-        guests = list(csv.reader(i.readlines()))
-
+def get_meetings_data(url, host_type):
+    meetings = []
+    r = requests.get(url)
+    
     if r.status_code == 200:
-        meetings = csv.reader(r.text.splitlines())
-        for meeting_row in meetings:
-            for guest_row in guests:
-                if meeting_row[6] == guest_row[1] and meeting_row[3] == datetime.today().strftime('%d/%m/%Y'):
+        meeting_table = etree.HTML(r.text).xpath('//table[@id="listMeetingsTable"]/tbody/tr')
+        for meeting in meeting_table:
+            if host_type == 'Commissioner':
+                hosts = [h.strip() for h in etree.HTML(r.text).xpath('(//nav/ol//li//a)[3]/text()')]
+                column_indices = {'date': '1', 'guests': '3'}
+            elif host_type == 'Cabinet':
+                hosts = [h.strip() for h in meeting.xpath('.//td[1]/text()')][:-1]
+                column_indices = {'date': '2', 'guests': '4'}
 
-                    if institution == 'European Commission':
-                        meeting = {
-                            'date'           : meeting_row[3],
-                            'institution'    : institution,
-                            'host'           : meeting_row[1],
-                            'public_body_id' : '576',
-                            'guest'          : guest_row[2] if guest_row[2] != '' else meeting_row[7],
-                            'url'            : 'https://www.integritywatch.eu/'
-                        }
-            
-                    elif institution == 'European Parliament':
-                        meeting = {
-                            'date'           : meeting_row[10],
-                            'institution'    : institution,
-                            'host'           : meeting_row[0],
-                            'public_body_id' : '546',
-                            'guest'          : meeting_row[4],
-                            'url'            : 'https://www.integritywatch.eu/mepmeetings'
-                        }
+            guests = []
+            for guest in meeting.xpath('.//td[{guests}]/comment()'.format(**column_indices)):
+                guest = etree.HTML('<' + str(guest).replace('<!-- ', '').replace('-->', ''))
+                guests.append({
+                    'name': guest.xpath('.//a/text()')[0],
+                    'id': urllib.parse.parse_qs(urllib.parse.urlsplit(guest.xpath('.//a/@href')[0]).query)['id'][0]
+                })
 
-                    send_confirmation_email(
-                        subject = 'New meeting with {guest}'.format(**meeting),
-                        body =
-                            '<html><body>' + \
-                            os.environ['TWEET_TEMPLATE'].format(**meeting) + \
-                            '<br/><br/>' + \
-                            '<a href="https://' + os.environ['API_ID'] + '.execute-api.eu-west-1.amazonaws.com/dev?' + \
-                            urllib.parse.urlencode(meeting) + '">' + \
-                            'Click here to send this Tweet and submit an FOI request!</a> If you don\'t think it\'s right, just ignore this email.' + \
-                            '</body></html>')
+            meetings.append({
+                'date': meeting.xpath('.//td[{date}]/text()'.format(**column_indices))[0].strip(),
+                'hosts': hosts,
+                'guests': guests,
+                'public_body_id': '576'
+            })
+   
+    return meetings
 
 def send_confirmation_email(subject, body):
     response = boto3.client('ses').send_email(
@@ -60,5 +43,41 @@ def send_confirmation_email(subject, body):
         Message = {
             'Subject': {'Data': subject},
             'Body': {'Html': {'Data': body} } })
-
+    
     return response
+
+def lambda_handler(event, context):
+    r = requests.get('https://ec.europa.eu/commission/commissioners/2019-2024_en')
+    commissioner_urls = etree.HTML(r.text).xpath('//h3[@class="listing__title"]/a/@href')
+    for url in commissioner_urls:
+        meetings = []
+        r = requests.get(url)
+        meetings += get_meetings_data(etree.HTML(r.text).xpath('//div[@id="transparency"]//a/@href')[0], 'Commissioner')
+        meetings += get_meetings_data(etree.HTML(r.text).xpath('//div[@id="transparency"]//a/@href')[1], 'Cabinet')
+
+        with open('data/targets.csv', 'r') as i:
+            target_rows = list(csv.reader(i.readlines()))
+            targets = {}
+            for row in target_rows:
+                targets[row[1]] = row[2]
+
+        for meeting in meetings:
+            hit = False
+            for target in targets.keys():
+                if target in [g['id'] for g in meeting['guests']]: # and meeting['date'] == '13/01/2021': #datetime.today().strftime('%d/%m/%Y'):
+                    hit = True
+        
+            if hit == True:
+                meeting['guests_string'] = join_with_and([targets.get(g['id']) for g in meeting['guests'] if targets.get(g['id']) is not None])
+                meeting['hosts_string'] = join_with_and(meeting['hosts'])
+                
+                send_confirmation_email(
+                    subject = 'New meeting with {guests_string}'.format(**meeting),
+                    body =
+                        '<html><body>' + \
+                        os.environ['TWEET_TEMPLATE'].format(**meeting) + \
+                        '<br/><br/>' + \
+                        '<a href="https://' + os.environ['API_ID'] + '.execute-api.eu-west-1.amazonaws.com/dev?' + \
+                        urllib.parse.urlencode(meeting) + '">' + \
+                        'Click here to send this Tweet and submit an FOI request!</a> If you don\'t think it\'s right, just ignore this email.' + \
+                        '</body></html>')
